5a152523624b3fdd017d02f0754951bbaec7aa6f
Alpha Lux <alphaluxtt@gmail.com>
2026-03-06 16:13:32 +0800

    fix booking prop

--- app/page.tsx
+++ app/page.tsx
@@ -676,7 +676,7 @@ function AboutPage({ setCurrentPage }: PageProps) {
   );
 }
 
-function BookingPage() {
+return <BookingPage setCurrentPage={setCurrentPage} />
   return (
     <div className="mx-auto max-w-7xl px-6 py-16 lg:px-10 lg:py-20">
       <SectionTitle
